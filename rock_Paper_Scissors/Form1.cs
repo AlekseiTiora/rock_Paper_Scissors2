d873844b72a5d9257c9fe3e0fadd88222d95ca8d
Aleksei Tiora <aleksei.tiora@gmail.com>
2021-12-08 19:13:27 +0200

    Добавьте файлы проекта.

---
+++ rock_Paper_Scissors/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace rock_Paper_Scissors
+{
+    public partial class Form1 : Form
+    {
+        Button btn;
+        Button btn2;
+        Button btn3;
+        PictureBox pb;
+        public Form1()
+        {
+            this.Height = 700;//высота
+            this.Width = 900;//ширина
+            this.Text = "Rock_Paper_Scissors";//загловок формы
+            this.BackgroundImage = new Bitmap(@"../../img/fon.jpg"); //картина фона формы
+
+            MainMenu menu = new MainMenu();
+            MenuItem menuFile = new MenuItem("Настройки");
+            menuFile.MenuItems.Add("Teema", new EventHandler(menuFile_Tema_Select));
+            menu.MenuItems.Add(menuFile);
+            this.Menu = menu;
+
+
+            btn = new Button();
+            btn.BackColor = Color.White;
+            btn.Text = "Play with bot";
+            btn.Location = new Point(Top = 250, 120);
+            btn.Size = new Size(100, 30);
+            btn.Click += Btn_Click;
+
+            btn2 = new Button();
+            btn2.BackColor = Color.White;
+            btn2.Text = "Two player";
+            btn2.Location = new Point(Top = 490, 120);
+            btn2.Size = new Size(100, 30);
+            btn2.Click += Btn2_Click;
+
+            btn3 = new Button();
+            btn3.BackColor = Color.White;
+            btn3.Text = "määrused";
+            btn3.Location = new Point(Top = 380, 430);
+            btn3.Size = new Size(90, 90);
+            btn3.Click += Btn3_Click;
+
+
+
+
+
+            //pictureBox
+            pb = new PictureBox();
+            pb.Size = new Size(200, 200);
+            pb.Location = new Point(320, 200);
+            pb.SizeMode = PictureBoxSizeMode.StretchImage;
+            pb.ImageLocation = ("../../img/igra.png");
+
+
+
+            this.Controls.Add(pb);
+            this.Controls.Add(btn3);
+            this.Controls.Add(btn2);
+            this.Controls.Add(btn);
+        }
+
+        private void Btn3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Võitja selgitatakse välja järgmiste reeglite järgi: Paber võidab kivi (paber keerab ümber kivi). Kivi lööb kääre (kivi tuhmub või lõhub käärid). Käärid peksid paberit (käärid lõikasid paberit).", "määrused");
+        }
+
+        int scetcik = 0;
+        private void menuFile_Tema_Select(object sender, EventArgs e)
+        {
+            scetcik++;
+            if (scetcik == 1)
+            {
+                this.BackgroundImage = new Bitmap(@"../../img/fon2.png");
+
+            }
+            else if (scetcik == 2)
+            {
+                this.BackgroundImage = new Bitmap(@"../../img/fon.jpg");
+                scetcik = 0;
+            }
+        }
+
+        private void Btn2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Btn_Click(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2();
+            form2.Show();
+        }
+    }
+}
---
+++ rock_Paper_Scissors/Form2.cs
@@ -0,0 +1,293 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace rock_Paper_Scissors
+{
+    public partial class Form2 : Form
+    {
+        Button btn;
+        Button btn2;
+        Button btn3;
+        TextBox txt;
+        Label lbl_p;
+        Label lbl_bot;
+        RadioButton rb;
+        RadioButton rb2;
+        RadioButton rb3;
+        PictureBox pb;
+        public int numb;
+        public Form2()
+        {
+            this.Size = new Size(500, 500);
+            this.Text = "Rock_Paper_Scissors";
+            this.BackgroundImage = new Bitmap(@"../../img/fon.jpg"); //картина фона формы
+
+            Label lbl = new Label();
+            lbl.Text = "kirjuta oma nickName:";
+            lbl.Location = new Point(190, 70);
+            lbl.Size = new Size(125, 15);
+            lbl.BackColor = Color.White;
+
+            txt = new TextBox();
+            txt.Location = new Point(125, 90);
+            txt.Size = new Size(250, 30);
+
+            Button btn = new Button();
+            btn.Size = new Size(150, 30);
+            btn.Location = new Point(170, 120);
+            btn.Text = "Edasi";
+            btn.Click += Btn_Click;
+
+            lbl_p = new Label();
+            lbl_p.Location = new Point(240, 70);
+            lbl_p.Font = new Font("Arial", 22, FontStyle.Bold);
+            lbl_p.BackColor = Color.White;
+            lbl_p.ForeColor = Color.Black;
+            lbl_p.TextChanged += Lbl_p_TextChanged;
+
+            lbl_bot = new Label();
+            lbl_bot.Text = "BOT";
+            lbl_bot.Location = new Point(560, 70);
+            lbl_bot.Size = new Size(100, 30);
+            lbl_bot.Font = new Font("Arial", 20, FontStyle.Bold);
+            lbl_bot.BackColor = Color.White;
+            lbl_bot.ForeColor = Color.Black;
+
+            rb = new RadioButton();
+            rb.Image = Image.FromFile(@"../../img/kivi.png");
+            rb.Location = new Point(50,120);
+            rb.Size = new Size(200, 200);
+
+            rb2 = new RadioButton();
+            rb2.Image = Image.FromFile(@"../../img/paber.png");
+            rb2.Location = new Point(50, 350);
+            rb2.Size = new Size(200, 200);
+
+            rb3 = new RadioButton();
+            rb3.Image = Image.FromFile(@"../../img/kaarid.png");
+            rb3.Location = new Point(50, 570);
+            rb3.Size = new Size(200, 200);
+
+            pb = new PictureBox();
+            pb.ImageLocation = "../../img/vopros.jpg";
+            pb.Size = new Size(200, 200);
+            pb.Location = new Point(500, 200);
+            pb.SizeMode = PictureBoxSizeMode.StretchImage;
+
+            btn2 = new Button();
+            btn2.Location = new Point(600, 500);
+            btn2.Size = new Size(100, 50);
+            btn2.Text = "Alusta";
+            btn2.MouseClick += Btn2_MouseClick; ;
+
+            List<string> list = new List<string>();
+
+            list.Add("kivi.png");
+            list.Add("paber.png");
+            list.Add("kaarid.png");
+
+            Random rnd = new Random();
+
+            int num = rnd.Next(3);
+            
+            btn3 = new Button();
+            btn3.Location = new Point(300, 500);
+            btn3.Size = new Size(100, 50);
+            btn3.MouseClick += Btn3_MouseClick;
+            btn3.Text = "Bot liigub";
+
+
+            this.Controls.Add(lbl);
+            this.Controls.Add(txt);
+            this.Controls.Add(btn);
+        }
+
+        private void Btn3_MouseClick(object sender, MouseEventArgs e)
+        {
+            pb.SizeMode = PictureBoxSizeMode.AutoSize;
+            List<string> lists = new List<string>();
+
+            lists.Add("kivi.png");
+            lists.Add("paber.png");
+            lists.Add("kaarid.png");
+            Random rand = new Random();
+
+            numb = rand.Next(3);
+            pb.ImageLocation = ($"../../images/{lists[numb]}");
+            btn2.Hide();
+            if (rb.Checked)
+            {
+                rb2.Hide();
+                rb3.Hide();
+            }
+            else if (rb2.Checked)
+            {
+                rb.Hide();
+                rb3.Hide();
+            }
+            else if (rb3.Checked)
+            {
+                rb2.Hide();
+                rb.Hide();
+            }
+
+        }
+
+        private void Btn2_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (rb.Checked == true && numb == 1 || rb2.Checked == true && numb == 2 || rb3.Checked == true && numb == 0)
+            {
+
+                var answer = MessageBox.Show(
+                "Sina võitis " + numb.ToString(),
+                "Sõnum",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Information,
+                MessageBoxDefaultButton.Button1,
+                MessageBoxOptions.DefaultDesktopOnly);
+                if (answer == DialogResult.Yes)
+                {
+                    Form2 op = new Form2();
+                    op.Show();
+                    op.WindowState = FormWindowState.Minimized;
+                    op.WindowState = FormWindowState.Normal;
+                    this.Hide();
+
+                }
+                else
+                {
+                    Form1 fp = new Form1();
+                    fp.Show();
+                    fp.WindowState = FormWindowState.Minimized;
+                    fp.WindowState = FormWindowState.Normal;
+                    this.Hide();
+
+                }
+
+
+            }
+            else if (rb.Checked == true && numb == 0 || rb2.Checked == true && numb == 1 || rb3.Checked == true && numb == 2)
+            {
+                var answer = MessageBox.Show(
+                "Ничья" + numb.ToString(),
+                "Sõnum",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Information,
+                MessageBoxDefaultButton.Button1,
+                MessageBoxOptions.DefaultDesktopOnly);
+                if (answer == DialogResult.Yes)
+                {
+                    Form2 op = new Form2();
+                    op.Show();
+                    op.WindowState = FormWindowState.Minimized;
+                    op.WindowState = FormWindowState.Normal;
+                    this.Hide();
+
+                }
+                else
+                {
+                    Form1 fp = new Form1();
+                    fp.Show();
+                    fp.WindowState = FormWindowState.Minimized;
+                    fp.WindowState = FormWindowState.Normal;
+                    this.Hide();
+
+                }
+
+            }
+            else
+            {
+                var answer = MessageBox.Show(
+                "Sa kaotasid, bot võitis",
+                "Sõnum",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Information,
+                MessageBoxDefaultButton.Button1,
+                MessageBoxOptions.DefaultDesktopOnly);
+                if (answer == DialogResult.Yes)
+                {
+                    Form2 op = new Form2();
+                    op.Show();
+                    op.WindowState = FormWindowState.Minimized;
+                    op.WindowState = FormWindowState.Normal;
+                    this.Hide();
+
+                }
+                else
+                {
+                    Form1 fp = new Form1();
+                    fp.Show();
+                    fp.WindowState = FormWindowState.Minimized;
+                    fp.WindowState = FormWindowState.Normal;
+                    this.Hide();
+
+                }
+
+
+            }
+        }
+
+
+
+        private void Rb_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void Btn_Click(object sender, EventArgs e)
+        {
+            this.Controls.Clear();
+
+            List<string> list = new List<string>();  // List of random NickName
+            list.Add("draken");
+            list.Add("maiky");
+
+
+            Random rnd = new Random();
+            int numb = rnd.Next(list.Count);
+
+
+
+            using (StreamWriter writer = new StreamWriter(@"../../player.txt"))  // Write file using StreamWriter
+            {
+                writer.WriteLine(txt.Text);
+            }
+
+            this.Size = new Size(900, 900);
+
+            string readText = File.ReadAllText(@"../../player.txt");  // Read file 
+            if (String.IsNullOrEmpty(txt.Text))
+            {
+                lbl_p.Text = list[numb];
+            }
+            else
+            {
+                lbl_p.Text = readText;
+            }
+
+            this.Controls.Add(lbl_p);
+            this.Controls.Add(lbl_bot);
+            this.Controls.Add(rb);
+            this.Controls.Add(rb2);
+            this.Controls.Add(rb3);
+            this.Controls.Add(pb);
+            this.Controls.Add(btn3);
+            this.Controls.Add(btn2);
+
+
+        }
+        private void Lbl_p_TextChanged(object sender, EventArgs e)
+        {
+            Size size_p = TextRenderer.MeasureText(lbl_p.Text, lbl_p.Font);
+            lbl_p.Size = new Size(size_p.Width, size_p.Height);
+        }
+    }
+}
